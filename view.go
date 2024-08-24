jmkim/gocui

commit 3fd13166c9c82fa7a696a3eb6a674b615acd0ae5

Make sure viewLines are up to date before looking at them
They are usually updated in draw(), but in lazygit we're about to add a call to
this function in layout, which is at a time where the view is already tainted
but hasn't been drawn yet.

view.go
@@ -1316,6 +1316,7 @@ func (v *View) LinesHeight() int {
 
 // ViewLinesHeight is the count of view lines (i.e. lines including wrapping)
 func (v *View) ViewLinesHeight() int {
+	v.refreshViewLinesIfNeeded()
 	return len(v.viewLines)
 }
 

commit 6bfac15cb7680ecd3135e3cc7d32b409fdb15a98

Fix rendering regression
If ViewLinesHeight was called concurrently with drawing the view, the viewLines
could be truncated.

Regression introduced in 3fd13166c9.

view.go
@@ -1316,6 +1316,9 @@ func (v *View) LinesHeight() int {
 
 // ViewLinesHeight is the count of view lines (i.e. lines including wrapping)
 func (v *View) ViewLinesHeight() int {
+	v.writeMutex.Lock()
+	defer v.writeMutex.Unlock()
+
 	v.refreshViewLinesIfNeeded()
 	return len(v.viewLines)
 }